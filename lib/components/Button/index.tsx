2f6de282951b237bb4f0de9bf41631cf7255cd94
Shane Smith <shane.r.smith@outlook.com>
2025-07-07 14:18:00 +0100

    setting up vite in library mode

---
+++ lib/components/Button/index.tsx
@@ -0,0 +1,4 @@
+export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { className, ...restProps } = props;
+  return <button className={`${className}`} {...restProps} />;
+}
