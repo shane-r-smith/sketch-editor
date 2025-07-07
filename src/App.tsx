0ed53e63432cb34e779c76130e342c432c43c22d
Shane Smith <shane.r.smith@outlook.com>
2025-07-07 14:22:13 +0100

    Setting up glob

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { Button } from "../lib/main";
+import { Button } from "../lib/components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
