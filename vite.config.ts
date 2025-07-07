7c71107e7f7f0e718f200e0df63963c2a348c7fa
Shane Smith <shane.r.smith@outlook.com>
2025-07-07 14:58:43 +0100

    Setting up tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import tailwindcss from "@tailwindcss/vite";
 
 import { extname, relative, resolve } from "path";
 import { fileURLToPath } from "node:url";
@@ -8,7 +10,7 @@ import { glob } from "glob";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] })],
+  plugins: [libInjectCss(), tailwindcss(), react(), dts({ include: ["lib"] })],
   build: {
     copyPublicDir: false,
     rollupOptions: {
