ea6a7f71edf314731127321f30a14d4790b1b01f
Shane Smith <shane.r.smith@outlook.com>
2025-08-17 22:57:26 +0100

    hex-colour-input

---
+++ lib/components/colour-picker/colour-inputs/hex-colour-input.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useState } from "react";
+import type { BaseColourPickerProps } from "../colour-picker.types";
+import { colord } from "colord";
+import TextField from "@mui/material/TextField";
+import FormLabel from "@mui/material/FormLabel";
+import Stack from "@mui/material/Stack";
+
+export function HexColourInput({
+  type,
+  colour,
+  onChange,
+  ...rest
+}: BaseColourPickerProps) {
+  const [currentColour, setCurrentColour] = useState(colord(colour).toHex());
+
+  useEffect(() => {
+    if (currentColour === colour) {
+      return;
+    }
+
+    const value = colord(colour);
+
+    if (value.isValid()) {
+      setCurrentColour(value.toHex());
+      return;
+    }
+
+    setCurrentColour(colour);
+    return;
+  }, [colour, currentColour]);
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCurrentColour(event.target.value);
+      onChange(event.target.value, type);
+    },
+    [onChange, type]
+  );
+
+  return (
+    <Stack>
+      <FormLabel>HEX</FormLabel>
+      <TextField
+        {...rest}
+        value={currentColour}
+        onChange={handleChange}
+        size="small"
+      />
+    </Stack>
+  );
+}
