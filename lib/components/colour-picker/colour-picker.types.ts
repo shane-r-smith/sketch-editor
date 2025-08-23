461115d19ad5b747f28e7418b836bbe3b9772345
Shane Smith <shane.r.smith@outlook.com>
2025-08-23 17:31:19 +0100

    add colour picker type definitions and interfaces

---
+++ lib/components/colour-picker/colour-picker.types.ts
@@ -0,0 +1,56 @@
+export type ColourType = "HEX" | "RGB";
+
+export interface BaseColourPickerProps {
+  /**
+   * The initial colour value for the colour picker.
+   * This is the colour that will be displayed when the picker is first rendered.
+   */
+  colour: string;
+
+  /**
+   * Type of colour input to use.
+   * This can be "HEX" for hexadecimal input or "RGB" for RGB input.
+   */
+  type: ColourType;
+
+  /**
+   * Callback function to handle colour changes.
+   * This function is called when the user selects a colour.
+   * @param colour The selected colour value.
+   * @param type The type of colour (e.g., HEX, RGB).
+   */
+  onChange: (colour: string, type: ColourType) => void;
+}
+
+export interface ColourPickerHistoryProps {
+  /**
+   * History of colours used in the picker.
+   * This is used to show the last used colours
+   * and allow users to quickly select them.
+   */
+  history: string[];
+
+  /**
+   * Number of history items to show in the picker.
+   * This is used to limit the number of items shown.
+   */
+  numHistoryToShow: number;
+
+  onColourSelected: (colour: string) => void;
+}
+
+export interface ColourPickerProps
+  extends BaseColourPickerProps,
+    ColourPickerHistoryProps {
+  /**
+   * Label for the colour picker.
+   * This is used to describe the purpose of the picker.
+   */
+  label: string;
+
+  /**
+   * Optional description for the colour picker.
+   * This can be used to provide additional context or instructions.
+   */
+  helperText?: string;
+}
