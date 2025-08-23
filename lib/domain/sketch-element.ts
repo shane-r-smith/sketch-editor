cee375e01e9f1dcbbd9abe71b8b979206c300bb4
Shane Smith <shane.r.smith@outlook.com>
2025-08-23 21:22:26 +0100

    refactor: simplify interface definitions for PenSketchElement and EraserSketchElement

--- lib/domain/sketch-element.ts
+++ lib/domain/sketch-element.ts
@@ -2,10 +2,11 @@ import type { EraserTool, PenTool } from "./tool";
 
 export type SketchElement = PenSketchElement | EraserSketchElement;
 
-export interface PenSketchElement extends PenTool {
+export interface PenSketchElement
+  extends Omit<PenTool, "icon" | "strokeColourHistory"> {
   points: number[];
 }
 
-export interface EraserSketchElement extends EraserTool {
+export interface EraserSketchElement extends Omit<EraserTool, "icon"> {
   points: number[];
 }
