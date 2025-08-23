3727cf2c7c540d5b014a38034f268c0b51f157c4
Shane Smith <shane.r.smith@outlook.com>
2025-08-23 21:22:11 +0100

    refactor: improve documentation and property naming in tool interfaces

--- lib/domain/tool.ts
+++ lib/domain/tool.ts
@@ -1,29 +1,35 @@
 import type { ColourType } from "./colour";
+import type { ReactNode } from "react";
 
+/** Represents the unique keys for different drawing tools. */
 export type ToolKey = "PEN" | "ERASER";
 
 /**
  * Represents the base properties shared by all drawing tools.
- *
- * @property key - Unique identifier for the tool.
- * @property stroke - The points.
- * @property strokeWidth - The width of the tool's stroke.
  */
 export interface BaseTool {
+  /** Identifier for the tool. */
   key: ToolKey;
+
+  /** The icon displayed for the tool. */
+  icon: ReactNode;
+
+  /** The points */
   stroke: string;
+
+  /** The width of the pen stroke. */
   strokeWidth: number;
 }
 
 /**
  * Represents a tool with an associated stroke colour and history.
- *
- * @property strokeType - The type of colour used for the stroke.
- * @property strokeHistory - An array of strings representing the history of stroke colours.
  */
 export interface ToolWithColour {
-  strokeType: ColourType;
-  strokeHistory: string[];
+  /** The type of colour used for the stroke. */
+  strokeColourType: ColourType;
+
+  /** An array of strings representing the history of stroke colours. */
+  strokeColourHistory: string[];
 }
 
 /** TOOLS */
@@ -31,21 +37,17 @@ export interface ToolWithColour {
 /**
  * Represents the pen tool used in the sketch editor.
  * Extends the {@link BaseTool} and includes {@link ToolWithColour}.
- *
- * @property key - The unique identifier for the pen tool ("PEN").
- * @property strokeWidth - The width of the pen stroke.
  */
 export interface PenTool extends BaseTool, ToolWithColour {
+  /** The unique identifier for the pen tool */
   key: "PEN";
 }
 
 /**
  * Represents the eraser tool used in the sketch editor.
  * Extends {@link BaseTool} and provides additional properties specific to erasing functionality.
- *
- * @property key - The unique identifier for the eraser tool, always "ERASER".
- * @property strokeWidth - The width of the eraser stroke in pixels.
  */
 export interface EraserTool extends BaseTool {
+  /** The unique identifier for the eraser tool */
   key: "ERASER";
 }
