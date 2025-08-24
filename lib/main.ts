7b2c1f9e632d4c92426c5ac7e73f42f6f35037e5
Shane Smith <shane.r.smith@outlook.com>
2025-08-24 15:26:45 +0100

    feat: re-export api module from main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -3,5 +3,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+export * from "./api";
 export * from "./components";
 export * from "./domain";
