bcfbfae8bb07421255ce1fc76208f85e8c866f47
Shane Smith <shane.r.smith@outlook.com>
2025-08-27 20:42:40 +0100

    adding side-panel exports

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./canvas";
 export * from "./colour-picker";
 export * from "./drawable-canvas";
+export * from "./side-panel";
 export * from "./sketch-editor";
 export * from "./sketch-provider";
 export * from "./workspace";
