2b5bf41264801e5801869d44f5bdd90499990e90
Shane Smith <shane.r.smith@outlook.com>
2025-08-27 20:21:49 +0100

    workspace

--- lib/components/workspace/workspace.tsx
+++ lib/components/workspace/workspace.tsx
@@ -1,92 +1,81 @@
-import { useAtomValue } from "jotai";
-import { sketchAtom } from "../../api/sketch.state";
+import { useAtomValue, useSetAtom } from "jotai";
+import {
+  addElementAtom,
+  currentLayerIndexAtom,
+  currentLayersAtom,
+  sketchAtom,
+} from "../../api/sketch-api/sketch-api.state";
+import { DrawableCanvas } from "../drawable-canvas";
+import { useCallback, useRef } from "react";
+import type { Stage } from "konva/lib/Stage";
+import type { DrawingState, SketchElement } from "../../domain";
+import { drawingAtom } from "../../api/drawing-api/drawing-api.state";
+import { publish } from "../../api/events-api";
+import Stack from "@mui/material/Stack";
 
-export function Workspace({ ...rest }) {
+export function Workspace() {
+  const canvasRef = useRef<Stage>(null);
   const sketch = useAtomValue(sketchAtom);
+  const layers = useAtomValue(currentLayersAtom);
+  const currentLayerIndex = useAtomValue(currentLayerIndexAtom);
+  const addElement = useSetAtom(addElementAtom);
+  const setDrawing = useSetAtom(drawingAtom);
 
-  // const layers = useAppSelector(selectLayers);
-  // const layer = useAppSelector(selectLayer);
-  // const pageIndex = useAppSelector(selectPageIndex);
-  // const layerIndex = useAppSelector(selectLayerIndex);
+  const _handleDraw = (element: SketchElement) => {
+    // Add to store
+    addElement(element);
 
-  // const [showCursor, setShowCursor] = useState(false); // initiate it at false
-  // const [loaded, setLoaded] = useState(false);
+    // Call editor change events
+    publish("SKETCH");
+  };
 
-  // const tool = useAppSelector(selectTool);
-  // const canvasRef = useRef<Stage>(null);
-  // const zoomRef = useRef(null);
+  const _handleDrawing = useCallback(
+    (drawingState: DrawingState) => {
+      setDrawing(drawingState);
+    },
+    [setDrawing]
+  );
 
-  // useEffect(() => {
-  //   if (loaded) {
-  //     return;
-  //   }
-
-  //   if (!sketch || !layers || !layer) {
-  //     return;
-  //   }
-
-  //   setLoaded(true);
-  // }, [loaded, sketch, layers, layer]);
-
-  // const _handleDraw = (element: EditorElement) => {
-  //   // add to store
-  //   dispatch(addElement({ element, pageIndex, layerIndex }));
-
-  //   // call editor change events
-  //   publish("change");
-
-  //   // Update stroke history
-  //   if ("strokeHistory" in tool) {
-  //     dispatch(updateStrokeHistory(element.stroke));
-  //     publish("tool-change");
-  //   }
-  // };
-
-  // const _handleDrawing = (drawingState: DrawingState) => {
-  //   dispatch(updateDrawingState(drawingState));
-  // };
-
-  // if (!loaded || !flipbook || !layers || !layer) {
-  //   return null;
-  // }
-
-  // return (
-  //   <div
-  //     className={"flipbook-canvas w-full h-full overflow-auto	bg-gray-200"}
-  //     onMouseEnter={() => {
-  //       setShowCursor(true);
-  //     }}
-  //     onMouseLeave={() => {
-  //       setShowCursor(false);
-  //     }}
-  //     {...rest}
-  //   >
-  //     {showCursor ? <Cursor /> : null}
-  //     <div
-  //       ref={zoomRef}
-  //       role="presentation"
-  //       className="flipbook-canvas-container w-full h-full flex justify-center items-center overflow-hidden bg-gray-200"
-  //     >
-  //       <DrawableCanvas
-  //         ref={canvasRef}
-  //         tool={tool}
-  //         layers={layers}
-  //         onionSkinningLayers={onionSkinningLayers}
-  //         selectedLayerIndex={layer.index}
-  //         disabled={!layer.visible || layer.locked}
-  //         onDraw={_handleDraw}
-  //         onDrawing={_handleDrawing}
-  //         width={flipbook.size[0]}
-  //         height={flipbook.size[1]}
-  //         className="bg-white border border-solid border-gray-500"
-  //       />
-  //     </div>
-  //   </div>
-  // );
+  if (!sketch || !layers) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div {...rest}>
-      <pre>{JSON.stringify(sketch, undefined, 2)}</pre>
-    </div>
+    <Stack width="100%" height="100%" bgcolor="grey.200" overflow="auto">
+      <Stack
+        direction="row"
+        width="100%"
+        height="100%"
+        justifyContent="center"
+        alignItems="center"
+        overflow="hidden"
+        bgcolor="grey.20"
+        role="presentation"
+      >
+        <DrawableCanvas
+          ref={canvasRef}
+          // TODO change tool
+          tool={{
+            tool: "PEN",
+            stroke: "#FF0000",
+            strokeWidth: 10,
+            strokeColourType: "HEX",
+            strokeColourHistory: [],
+            icon: "pen",
+          }}
+          layers={layers}
+          currentLayerIndex={currentLayerIndex}
+          onDraw={_handleDraw}
+          onDrawing={_handleDrawing}
+          width={sketch.size[0]}
+          height={sketch.size[1]}
+          style={{
+            backgroundColor: "#FFF",
+            boxShadow:
+              "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
+          }}
+        />
+      </Stack>
+    </Stack>
   );
 }
