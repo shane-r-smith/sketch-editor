6c2014a89c0663c92edefa4390b3ba399784ac00
Shane Smith <shane.r.smith@outlook.com>
2025-08-27 20:03:52 +0100

    exports

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./canvas";
 export * from "./colour-picker";
 export * from "./drawable-canvas";
+export * from "./sketch-editor";
 export * from "./sketch-provider";
 export * from "./workspace";
